f03fcc398b989e904b7597e97a2603f02175ad76
AlisonWonderland <ally@lilypad.tech>
2026-01-24 21:54:30 +1100

    add a quote component

--- snippets/components/display/quote.jsx
+++ snippets/components/display/quote.jsx
@@ -1,3 +1,6 @@
+import { Frame } from "./frame.jsx";
+import { Icon } from "mintlify/components";
+
 export const Quote = ({ children }) => {
   const quoteStyle = {
     fontSize: "1rem",
@@ -11,4 +14,79 @@ export const Quote = ({ children }) => {
     margin: "1rem 0",
   };
   return <div style={quoteStyle}>{children}</div>;
+};
+
+export const FrameQuote = ({
+  children,
+  author,
+  source,
+  href,
+  frame = true,
+  img,
+  ...props
+}) => {
+  const content = (
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        padding: "0.75rem 1rem 0.25rem 1rem",
+        gap: "0.1rem",
+      }}
+    >
+      <div
+        style={{
+          borderLeft: "4px solid var(--accent)",
+          paddingLeft: "1rem",
+          fontStyle: "italic",
+        }}
+      >
+        {children}
+      </div>
+      {(author || source) && (
+        <div style={{ display: "flex", justifyContent: "flex-end" }}>
+          <div style={{ textAlign: "left" }}>
+            {author && (
+              <div>
+                <Icon icon="microphone" />{" "}
+                <strong>
+                  <em>{author}</em>
+                </strong>
+              </div>
+            )}
+            {source &&
+              (href ? (
+                <a href={href} target="_blank">
+                  <span
+                    style={{
+                      opacity: 0.7,
+                      fontStyle: "italic",
+                      borderBottom: "1px solid var(--accent)",
+                    }}
+                  >
+                    {source}
+                  </span>{" "}
+                  <Icon icon="arrow-up-right" size={12} color="var(--accent)" />
+                </a>
+              ) : (
+                <span style={{ opacity: 0.7, fontStyle: "italic" }}>
+                  {source}
+                </span>
+              ))}
+          </div>
+        </div>
+      )}
+    </div>
+  );
+
+  return frame ? (
+    <div style={{ marginBottom: "2rem" }}>
+      <Frame {...props}>
+        {img && <img src={img.src} alt={img.alt} />}
+        {content}
+      </Frame>
+    </div>
+  ) : (
+    content
+  );
 };
