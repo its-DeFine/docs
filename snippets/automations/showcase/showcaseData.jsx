1578436ef3ada265a1a94255568c0e3236740442
Alison Haire <alihaire900@gmail.com>
2026-02-11 19:31:03 +1100

    Update showcase: 1 approved projects

--- snippets/automations/showcase/showcaseData.jsx
+++ snippets/automations/showcase/showcaseData.jsx
@@ -4,6 +4,7 @@ export const showcaseData = [
     "subtitle": "New York City’s free, independent 24/7 TV network",
     "href": "https://nytv.live",
     "mediaSrc": "https://drive.google.com/open?id=1M3II4tri1wB2h29qx9Sp4hSVOT2-7SFq",
+    "mediaType": "video",
     "logo": "https://drive.google.com/open?id=1t3aDictVaUtrFUKdraKRI5yksxpZvc3d",
     "categoryTags": [
       "Video Streaming",
