78e8d94500f4935b7730ef2bb4352aef3789be04
Alison Haire <alihaire900@gmail.com>
2026-02-11 13:57:42 +1100

    Update showcase: 1 approved projects

--- snippets/automations/showcase/showcaseData.jsx
+++ snippets/automations/showcase/showcaseData.jsx
@@ -1,1 +1,32 @@
-export const showcaseData = [];
+export const showcaseData = [
+  {
+    "title": "nytv.live",
+    "subtitle": "New York City’s free, independent 24/7 TV network",
+    "href": "https://nytv.live",
+    "mediaSrc": "https://drive.google.com/open?id=1M3II4tri1wB2h29qx9Sp4hSVOT2-7SFq",
+    "logo": "https://drive.google.com/open?id=1t3aDictVaUtrFUKdraKRI5yksxpZvc3d",
+    "categoryTags": [
+      "Video Streaming",
+      "Events & Communications"
+    ],
+    "productTags": [
+      "Livepeer Network"
+    ],
+    "description": "NYTV is a free, independent internet TV network that broadcasts live shows, documentaries, and news programs 24/7 from New York City.  It uses Livepeer for its video streaming infrastructure, ensuring low latency and high quality for its viewers, while keeping costs low",
+    "cta": "Watch now! : https://nytv.live/",
+    "links": [
+      {
+        "instagram": "https://instagram.com/newyorktelevision"
+      },
+      {
+        "bluesky": "https://bsky.app/profile/nytv.live"
+      },
+      {
+        "website": "https://nytv.live"
+      },
+      {
+        "email": "info@nytv.live"
+      }
+    ]
+  }
+];
